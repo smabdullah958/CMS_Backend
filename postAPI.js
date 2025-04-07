bbd9e039939ee883c2a36ab6f1f592a26fb2a6b7
abdullah <smabdulla958>
2025-04-07 17:42:29 +0500

    Fix missing dependencies

--- postAPI.js
+++ postAPI.js
@@ -3,7 +3,7 @@ process.env.Email_Pass="cmjy httq irvc tltz";
 console.log(process.env.Email_Pass,process.env.Email_User)
 
 let Express =require('express');
-let bcrypt = require('bcryptjs');
+let bcrypt = require('bcrypt');
 let Cors=require('cors');
 let JWT=require('jsonwebtoken');
  let cookieParser=require('cookie-parser');
